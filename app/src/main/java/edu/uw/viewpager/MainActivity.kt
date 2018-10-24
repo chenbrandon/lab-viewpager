0089cca6ae55d16e1bc9cdb65491654f5515de95
Brandon Chen <chenb777@uw.edu>
2018-10-24 13:09:45 -0700

    add innerclass MovePagerAdapter

--- app/src/main/java/edu/uw/viewpager/MainActivity.kt
+++ app/src/main/java/edu/uw/viewpager/MainActivity.kt
@@ -1,16 +1,29 @@
 package edu.uw.viewpager
 
 import android.os.Bundle
-import android.support.v4.app.FragmentTransaction
+import android.support.v4.app.*
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListener {
 
+    private lateinit var searchFragment: SearchFragment
+    private lateinit var detailFragment: DetailFragment
+    private lateinit var listFragment: ListFragment
+    private lateinit var viewPager: ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchFragment = SearchFragment.newInstance()
+        viewPager = findViewById<ViewPager>(R.id.pager)
+
+
+
     }
 
     //respond to search button clicking
@@ -34,6 +47,28 @@ class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListe
         ft.addToBackStack(null) //remember for the back button
         ft.commit()
     }
+    private inner class MoviePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment {
+            //Hard-code the ordering as an example
+            if (position == 0) return searchFragment
+            if (position == 1) return detailFragment
+        }
+
+        override fun getCount(): Int  {
+            return if (listFragment == null) {
+                1
+            } else if (detailFragment == null) {
+                2
+            } else {
+                3
+            }
+        }
+
+        override fun getItemPosition(`object`: Any?): Int {
+            return PagerAdapter.POSITION_NONE
+        }
+    }
+
 
     companion object {
 
